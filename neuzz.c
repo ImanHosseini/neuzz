80b685b24d611f078572db256e8ed747fd6e7b63
ImanHosseini <salvationsentinel@yahoo.com>
2021-01-27 07:32:24 -0500

    .

--- neuzz.c
+++ neuzz.c
@@ -344,7 +344,7 @@ void init_forkserver(char** argv) {
   int rlen;
   out_file = alloc_printf("%s/.cur_input", out_dir);
   printf("Spinning up the fork server...\n");
-  printf("NEW VERSION!\n");
+  printf("NEW VERSION 2!\n");
   perror("XXXXXXXXXXXXXXXXXXXXXXXXX\nX\nX\nX\nX\n");
   if (pipe(st_pipe) || pipe(ctl_pipe)) perror("pipe() failed");
 
